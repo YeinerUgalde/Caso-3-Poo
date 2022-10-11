a6f0cc0767e2d9325417c8480132837940a85d75
YeinerUgalde <103976664+YeinerUgalde@users.noreply.github.com>
2022-10-10 19:31:16 -0600

    Add files via upload

---
+++ Garden/Caso 3/src/Constantes/Constantes.java
@@ -0,0 +1,21 @@
+package Constantes;
+
+
+public class Constantes{
+	
+	public static float VelocidadDeCrecimienntoGeneral;
+	public static float VelocidadDelaSimulacion;
+	public static final int dias = 365;
+	
+	public static void setVelocidadDeCrecimienntoGeneral(float velocidadDeCrecimienntoGeneral) {
+		VelocidadDeCrecimienntoGeneral = velocidadDeCrecimienntoGeneral;
+	}
+
+	public static void setVelocidadDelaSimulacion(float velocidadDelaSimulacion) {
+		VelocidadDelaSimulacion = velocidadDelaSimulacion;
+	}
+
+	
+
+
+}
